dd5c0ea3da55ea03007bd580fb7358309014a79a
Alexseesmyname <skalpel550@gmail.com>
2020-09-22 20:19:11 +0300

    Added yandex map, used JS API

--- js/main.js
+++ js/main.js
@@ -12,3 +12,11 @@ var mySwiper = new Swiper(".swiper-container", {
   },
   effect: "fade",
 });
+
+ymaps.ready(init);
+function init() {
+  var myMap = new ymaps.Map("map", {
+    center: [41.013421, 28.978255],
+    zoom: 10,
+  });
+}
