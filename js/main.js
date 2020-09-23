65ca5b824153d078ba49db1360355cf654e4ee03
Alexseesmyname <skalpel550@gmail.com>
2020-09-23 20:29:31 +0300

    Added tablet, mobile responcivity for "review"

--- js/main.js
+++ js/main.js
@@ -23,24 +23,3 @@ var reviewSlider = new Swiper(".reviews-slider", {
     prevEl: ".reviews-slider__button_prev",
   },
 });
-
-ymaps.ready(init);
-function init() {
-  var myMap = new ymaps.Map("map", {
-      center: [41.013421, 28.978255],
-      zoom: 10,
-    }),
-    myGeoObject = new ymaps.GeoObject(
-      {
-        geometry: {
-          type: "Point",
-          coordinates: [41.013421, 28.978255],
-        },
-      },
-      {
-        preset: "islands#redIcon",
-      }
-    );
-
-  myMap.geoObjects.add(myGeoObject);
-}
