9bea69ac3f5828957e6aa91c94c2a3f7d6a032b5
Alexseesmyname <skalpel550@gmail.com>
2020-09-23 11:07:16 +0300

    Added map mark

--- js/main.js
+++ js/main.js
@@ -16,7 +16,23 @@ var mySwiper = new Swiper(".swiper-container", {
 ymaps.ready(init);
 function init() {
   var myMap = new ymaps.Map("map", {
-    center: [41.013421, 28.978255],
-    zoom: 10,
-  });
+      center: [41.013421, 28.978255],
+      zoom: 10,
+    }),
+    myGeoObject = new ymaps.GeoObject(
+      {
+        geometry: {
+          type: "Point",
+          coordinates: [41.013421, 28.978255],
+        },
+        properties: {
+          iconContent: "",
+        },
+      },
+      {
+        preset: "islands#redIcon",
+      }
+    );
+
+  myMap.geoObjects.add(myGeoObject);
 }
